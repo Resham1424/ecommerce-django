fe16790d5f991097b1f6f085f91735568001f642
Student-performancedetection <rresham317@gmail.com>
2025-10-03 17:09:40 +0530

    Prepare settings.py and render.yaml for Render deployment with Postgres and static files

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ----------------------------
 SECRET_KEY = config("SECRET_KEY", default="django-insecure-resham-secret-key")
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = ["smartshop-db-wxdd.onrender.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["resham-ecommerce-db.onrender.com", "127.0.0.1", "localhost"]
 
 # ----------------------------
 # INSTALLED APPS
